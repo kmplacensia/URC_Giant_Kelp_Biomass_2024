961ced76dbbda56b6f4451c3867aa8c849695ad1
zoekitchel <zoe.j.kitchel@gmail.com>
2024-06-24 14:09:05 -0700

    uploaded data and wrote template script for data analysis

---
+++ load_kelp_dive_data.R
@@ -0,0 +1,70 @@
+# CREATION DATE 24 June 2024
+# MODIFIED DATE 24 June 2024
+
+# AUTHOR: kitchel@oxy.edu
+
+# PURPOSE: Pull in CRANE data to calculate kelp density across PVR
+# NOTE: this script will only work for someone who is connected to VRG Dropbox on their person computer!!
+
+#############################
+##Setup
+#############################
+
+library(data.table)
+library(vegan)
+library(dplyr)
+
+############################
+#DROPBOX STEPS
+############################
+#SELECT <- "BOEM_depth_comparison"
+
+#############################
+##Load data
+#############################
+
+# Read in reference tables (conversions, station names, species names, codes for UPC, ISC, etc.)
+source("~/Dropbox/VRG Files/R Code/DataFiles/READ_Sites_Fish_BRS.r")
+
+#Loads up and processes CRANE data
+source("~/Dropbox/VRG Files/R Code/Integrated Dive General/CRANE_data_prep.R")
+
+CRANE_data_prep(SELECT = "BOEM_depth_comparison",
+                add_0s = "sp_0s", #summed to by Species with 0s added
+                )
+
+########################
+##Split out kelp
+########################
+#EVENT data
+#dat_event
+
+#KELP (SWATH)
+#swath_melt_T_sp, and then %in% "kelp - understory", "kelp - canopy" ,"Sargassum", "green algae", "giant kelp stipes"
+#      "giant kelp stipes" included
+dat_macrocystis <- swath_melt_T_sp %>%
+  filter(BenthicReefSpecies %in% c("Macrocystis pyrifera stipes", "Macrocystis pyrifera"))
+
+
+########################
+##Add Level Orders to Region, Site, DepthZone
+########################
+source("~/Dropbox/VRG Files/R Code/Integrated Dive General/CRANE_level_orders.R")
+
+dat_macrocystis <- CRANE_level_orders(dat_macrocystis, AR_Region = T, AR_Complex = T, Regions = T,
+                                      PVR_Monitor_Cat = T, KOU_SiteType = T, SiteType = T, DepthZones = T, Sites = T,
+                                      Eras = T) |>
+  droplevels()
+
+########################
+##Filtering to PV only
+########################
+
+dat_PV_macrocystis <- dat_macrocystis %>%
+  filter(Region == "Palos Verdes")
+
+########################
+##Save this subset as a CSV
+########################
+
+write_csv(dat_PV_macrocystis, file = file.path("data","dat_PV_macrocystis.csv"))
---
+++ scripts/explore_subset_data.R
@@ -0,0 +1,74 @@
+# CREATION DATE 24 June 2024
+# MODIFIED DATE 24 June 2024
+
+# AUTHOR: kitchel@oxy.edu & placensia@oxy.edu
+
+# PURPOSE: Explore the data on kelp at Palos Verdes, and subset to data we need for analysis
+
+#############################
+##Setup
+#############################
+
+library(tidyverse)
+
+############################
+#LOAD DATA
+############################
+dat_PV_macrocystis <- read_csv(file.path("data", "dat_PV_macrocystis.csv"))
+
+#Let's look at it!
+View(dat_PV_macrocystis)
+
+#Let's view some summary statistics
+summary(dat_PV_macrocystis)
+
+############################
+#Only keep artificial reefs at Palos Verdes
+############################
+
+#What types of sites did they survey?
+unique(dat_PV_macrocystis$DepthZone) #note ARM stands for artificial reefs
+
+#We only want years after installation of Palos Verdes (September of 2020)
+dat_PVR_macrocystis <- dat_PV_macrocystis |>
+  filter(DepthZone == "ARM") #only include sample years after 2020
+
+############################
+#Narrow down years?
+############################
+
+#What years do we have now?
+sort(unique(dat_PVR_macrocystis$SampleYear))
+
+#We only want years after installation of Palos Verdes (September of 2020)
+dat_PV_macrocystis_post_construction <-   #Try it yourself!! (follow above as a guide)
+
+
+
+############################
+#To calculate an overall density for each site and sampling day, we will sum macrocystis stipe count and plant count over 2 transects, and then divide by 120 meters squared (60 m x 2)
+############################
+
+#These 'cheat sheets' are always helpful for working through R! https://www.rstudio.com/wp-content/uploads/2015/02/data-wrangling-cheatsheet.pdf
+
+#First, stipe densities!
+
+PV_macrocystis_stipe_densities <- dat_PV_macrocystis_post_construction |>
+  group_by(SampleYear, SampleDate,Site) |> #take sum for each site, each sampling day, and each sampling year
+  filter(BenthicReefSpecies == "Macrocystis pyrifera stipes") |> #limit to stipes only!
+  summarise(stipe_density_m2 = sum(Abundance)/120)
+
+#take a look!
+View(PV_macrocystis_stipe_densities)
+
+#Second, plant densities!
+
+
+PV_macrocystis_plant_densities <- #Try it yourself!! (follow above as a guide)
+
+############################
+#How does density vary across years?
+############################
+ggplot(data = PV_macrocystis_stipe_densities ) +
+  geom_point(aes(x = factor(SampleYear), y = stipe_density_m2)) +
+  theme_classic()
